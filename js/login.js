71444d9ef7c82b88c93463a3ff7dda72687d2d66
mathias labora martirena <mathiaslabora@gmail.com>
2021-08-15 17:23:07 -0300

    todo hecho sin pulir
    falta confirmar con nico algunas cosas y mejorar codigo

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+const data = [];
 
+const buttonSend = () => {
+    const userText = document.getElementById('user');
+    const passText = document.getElementById('pass');
+    const user = userText.value;
+    const pass = passText.value;
+    if (!user || !pass) {
+        alert("Debe ingresar ambos campos!");
+    } else {
+        data.push({
+            user,
+            pass
+        })
+
+        localStorage.setItem("dataUser", JSON.stringify(data))
+        window.location.href = "index.html";//redirecciona la pag hacia index.html
+        userText.value = "";
+        passText.value = "";
+    }
+}
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("enviar").addEventListener("click", buttonSend);//ejecuta por medio del boton cuando click la funcion correspondiente
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,30 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
-});
+const createListOfProd = (products) => {
+    let listCreate = document.getElementById('listHtmlCollection');
+    for (let key of products) {
+        const lista = document.createElement('ul');
+        const li = document.createElement("li")
+        li.appendChild(document.createTextNode(`${key.name}`))
+        lista.appendChild(li)
+
+        let liDesc = document.createElement('ul');
+        liDesc.innerHTML = (`<li>Descripcion: ` + key.description + `</li>         
+        <li>Costo: `+ key.currency + ` ` + key.cost + `</li> 
+        <li>Vendidos: `+ key.soldCount + `</li> 
+        <li> <img src='`+ key.imgSrc + `' alt='imagen autos'></li> `)
+        lista.appendChild(liDesc)
+        listCreate.appendChild(lista)
+    }
+}
+document.addEventListener("DOMContentLoaded", async function (e) {
+    const obtDiv = document.getElementsByTagName('div')[1]; //guarda el div creado por jap.
+    obtDiv.innerHTML = ''; //borro contenido dentro del div
+    let createP = document.createElement('p');
+    obtDiv.appendChild(createP).id = 'listHtmlCollection'; /*creo un nuevo parrafo que contendra el listado 
+    y le doy un id para utilizarlo con mas facilidad*/
+    const products = (await getJSONData(PRODUCTS_URL)).data //traigo el json correspondiente
+    createListOfProd(products) //ejecuto la funcion para la creacion del listado
+})
