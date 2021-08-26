78d496f175da47a5b8744e78e3d4eec0ab764965
mathias labora martirena <mathiaslabora@gmail.com>
2021-08-26 20:20:22 -0300

    5.1

--- js/init.js
+++ js/init.js
@@ -46,5 +46,14 @@ if (parseRedir === null) {
   window.location.href = "login.html";
 }
 
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+  localStorage.clear();
+window.location.reload();
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
 });
--- js/login.js
+++ js/login.js
@@ -6,9 +6,8 @@ const userText = document.getElementById('user');
 const passText = document.getElementById('pass');
 
 const clean = () => {
-    sessionStorage.setItem("dataUser", JSON.stringify(data))//guarda array "data" en localstorage
-    window.location.href = "index.html";//redirecciona la pag hacia index.html
-    //limpia los espacios de usuario y contraseña.
+    sessionStorage.setItem("dataUser", JSON.stringify(data))
+    window.location.href = "index.html";
     userText.value = "";
     passText.value = "";
 }
@@ -28,6 +27,34 @@ const buttonSend = () => {
     }
 }
 
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+  
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    const user = profile.getGivenName();
+    const pass = profile.getFamilyName();
+    data.push({
+        user,
+        pass
+    })
+  sessionStorage.setItem("dataUser", JSON.stringify(data))//guarda array "data" en localstorage
+    window.location.href = "index.html";//redirecciona la pag hacia index.html
+    //limpia los espacios de usuario y contraseña.
+    userText.value = "";
+    passText.value = "";
+  }
+  
+  
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
