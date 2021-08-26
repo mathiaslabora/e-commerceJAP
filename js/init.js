f9b7224e5e2fddc6238323c1216af32602959895
mathias labora martirena <mathiaslabora@gmail.com>
2021-08-26 20:28:47 -0300

    5.4

--- js/init.js
+++ js/init.js
@@ -46,14 +46,6 @@ if (parseRedir === null) {
   window.location.href = "login.html";
 }
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-  localStorage.clear();
-window.location.reload();
-}
 
 document.addEventListener("DOMContentLoaded", function (e) {
 });
