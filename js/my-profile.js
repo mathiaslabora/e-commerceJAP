e010039b53c8e87a19a04549606d0db7d42a0f74
mathias labora martirena <mathiaslabora@gmail.com>
2021-10-22 11:26:28 -0300

    22/10

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,9 +1,65 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+"use strict";
+let nam = document.getElementById('name');
+let age = document.getElementById('age');
+let mail = document.getElementById('mail');
+let tel = document.getElementById('tel');
+let objSaveData = {};
+let catchDataObj;
+
+
+
+
+
+const putPlaceholder = () => {
+
+  if (catchDataObj) {
+    nam.value = catchDataObj.nombre;
+    age.value = catchDataObj.edad;
+    mail.value = catchDataObj.email;
+    tel.value = catchDataObj.telefono;
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
-    const parseRedir = JSON.parse(localStorage.getItem('dataUser'));
-    if (parseRedir === null) {
-      window.location.href = "login.html";
+
+  const parseRedir = JSON.parse(localStorage.getItem('dataUser'));
+  if (parseRedir === null) {
+    window.location.href = "login.html";
+  }
+  catchDataObj = JSON.parse(localStorage.getItem("dataObj"))
+  putPlaceholder();
+
+  //boton guardar, guarda en objeto y en localstorage
+  document.getElementById('save').addEventListener('click', () => {
+    if(nam.value === "" || age.value === "" || mail.value === ""){
+      document.getElementById('intMsj').innerHTML = `
+<div class="alert alert-danger" role="alert">
+  Ingrese los campos con * , son obligatorios!!
+</div>`
+    }else{
+    objSaveData = {
+      nombre: nam.value,
+      edad: age.value,
+      email: mail.value,
+      telefono: tel.value
     }
+    localStorage.setItem("dataObj", JSON.stringify(objSaveData))
+
+    document.getElementById('intMsj').innerHTML = `
+<div class="alert alert-primary" role="alert">
+  Datos guardados satifactoriamente!!
+</div>
+`
+  setTimeout(() => {
+document.getElementById('intMsj').innerHTML = "";
+}, 4000)
+    }
+  })
+
+
+
+
+
+
 });
--- js/products.js
+++ js/products.js
@@ -1,4 +1,5 @@
 
+"use strict";
 let defOrd = 'REL';
 let priceFrom;
 let priceTo;
@@ -6,11 +7,32 @@ let prodFilt;
 let listado = document.getElementById('listadoColeccion');
 const createListOfProd = (productos) => {
     listado.innerHTML = "";
-    for (let key of productos) {
-        const lista = document.createElement('a');
-        lista.href = "product-info.html";
-        lista.classList.add('list-group-item-action')
-        lista.innerHTML = `<div class="row prodSt">
+    const lista = document.createElement('div');
+    lista.classList.add('row')
+    for (let key of productos) {       
+        lista.innerHTML += `
+        
+        
+        <div class="col-md-4">
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+        <img src="` + key.imgSrc + `" alt="` + key.description + `" class="img-thumbnail">
+          <h3 class="m-3">`+ key.name + `</h3>
+          
+          <div class="card-body">
+          <small class="text-muted">` + key.soldCount + ` vendidos</small>
+            <p class="card-text">` + key.description + `</p>
+            <p class="mb-1">` + key.currency + ` ` + key.cost + `</p>
+          </div>
+        </a>
+      </div>
+           `
+        listado.appendChild(lista)
+            }
+            localStorage.setItem("arrayProductos", JSON.stringify(prodFilt))//guardo array de productos en localStorage
+}
+
+
+/* <div class="row prodSt">
         <div class="col-3">
             <img src="` + key.imgSrc + `" alt="` + key.description + `" class="img-thumbnail">
         </div>
@@ -22,11 +44,9 @@ const createListOfProd = (productos) => {
             <p class="mb-1">` + key.description + `</p><br>
             <p class="mb-1">` + key.currency + ` ` + key.cost + `</p>
         </div>
-    </div>`
-        listado.appendChild(lista)
-            }
-            localStorage.setItem("arrayProductos", JSON.stringify(prodFilt))//guardo array de productos en localStorage
-}
+    </div> */
+
+
 
 function ordPorPrecioYRel(array, defOrd) {
     defOrd === 'AZ' ? array.sort((a, b) => {
@@ -78,6 +98,18 @@ document.addEventListener("DOMContentLoaded", async function (e) {
             ordPorPrecioYRel(prodFilt, defOrd)
         }
     })
+
+//limpia filtro
+document.getElementById('cleanFilter').addEventListener('click', () => {
+    document.getElementById('from').value = "";
+    document.getElementById('to').value = "";
+    priceFrom = undefined;
+    priceTo = undefined;
+
+    ordPorPrecioYRel(prodFilt, defOrd)
+})
+
+
     //cambia valores en form de input -desde-
     document.getElementById('from').addEventListener('input', () => {
         priceFrom = document.getElementById('from').value;
